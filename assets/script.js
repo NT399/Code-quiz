8294b2d15c69128798cd22da48b21375d40b1eaf
NT399 <nimeasht@gmail.com>
2022-09-11 23:43:52 +1000

    updated html, css and JavaScript

--- assets/script.js
+++ assets/script.js
@@ -1,71 +1,94 @@
 var question = document.querySelector(".questions");
 var options = document.querySelector(".options");
-var timerDisplay = document.querySelector(".timer-count");
-var start = document.querySelector(".start-button");
 var option1 = document.querySelector("#option1");
 var option2 = document.querySelector("#option2");
 var option3 = document.querySelector("#option3");
 var option4 = document.querySelector("#option4");
+var option01 = document.querySelector("#option01");
+var option02 = document.querySelector("#option02");
+var option03 = document.querySelector("#option03");
+var option04 = document.querySelector("#option04");
+var option001 = document.querySelector("#option001");
+var option002 = document.querySelector("#option002");
+var option003 = document.querySelector("#option003");
+var option004 = document.querySelector("#option004");
 var outcome = document.querySelector("#outcome");
-
-var isWin = false;
-var timer;
-var timerCount;
+var timerScreen = document.querySelector(".timer-display");
+var highscores = []
 
 
 // Init function loads the page
 function init() {
-  getWins();
-  getlosses();
+  startQuiz();
+  getScore();
 }
 
-// The startGame function is called when the start button is clicked
+// The startQuiz function starts the page
 function startQuiz() {
-  isWin = false;
-  timerCount = 75;
-  // Prevents start button from being clicked when round is in progress
-  start.disabled = true;
+  timerCount = 50;
   firstQuestion()
   startTimer()
 }
 
-// The winGame function is called when the win condition is met
-function winGame() {
+// This notifies the user if their answer is correct
+function correctAnswer() {
   outcome.textContent = "Correct!";
-  start.disabled = false;
-  setWins()
 }
 
-// The loseGame function is called when timer reaches 0
-function loseGame() {
+// This notifies the user if their answer is incorrect and applies a time penalty
+function wrongAnswer() {
   outcome.textContent = "Wrong!";
-  start.disabled = false;
-  setLosses()
+  timerCount -= 10;
+  
 }
 
-// The setTimer function starts and stops the timer and triggers winGame() and loseGame()
+//Navigates to the scores page where you can register your initials
+function finalScore() {
+  document.getElementById("group3").style.display= "none";
+  clearInterval(timer);
+  question.textContent = "Your final score is " + timerCount;
+
+  document.getElementById("label").style.display= "block";
+  var nameInput = document.getElementById("input").style.display= "block";
+  nameInput.textContent= "input your initials"
+  nameInput
+
+  var submitButton = document.createElement("INPUT");
+  submitButton.setAttribute("type", "button");
+  submitButton.setAttribute("value", "Submit");
+  document.body.appendChild(submitButton);
+  submitButton.setAttribute("style", "width:70px; margin: 0 auto");
+
+  submitButton.addEventListener("click", setScore);
+}
+
+//Sets scores
+function setScore() {
+
+highscores.push(document.getElementById("input").value + " = " + timerCount);
+
+localStorage.setItem("score1",highscores);
+ 
+ getScore()
+}
+
+// Retrieves stored scores
+function getScore() {
+  var ScoreList = document.querySelector("#ScoreList");
+  localStorage.getItem("score1");
+  ScoreList.textContent = highscores; 
+}
+
+
+// This function starts the timer
 function startTimer() {
-  // Sets timer
   timer = setInterval(function() {
     timerCount--;
-    timerDisplay.textContent = "Timer: " + timerCount;
-    if (timerCount >= 0) {
-      // Tests if win condition is met
-      if (isWin && timerCount > 0) {
-        // Clears interval and stops timer
-        winGame();
-      }
-    }
-    // Tests if time has run out
+    timerScreen.textContent = "Timer: " + timerCount;
+    // If the timer runs out, it goes to the final scores page.
     if (timerCount === 0) {
-      // Clears interval
       clearInterval(timer);
-      loseGame();
-    }
-    //can remove...
-    if (isWin = false && timerCount > 0) {
-      // Clears interval and stops timer
-      loseGame();
+      finalScore();
     }
   }, 1000);
 }
@@ -73,71 +96,98 @@ function startTimer() {
 // First question 
 function firstQuestion() {
 
-  question.textContent = "1+1 = ";
-  option1.textContent = "1";
-  option2.textContent = "2";
-  option3.textContent = "3";
-  option4.textContent = "4";
+  //Question and options
+  document.getElementById("group1").style.display= "block";
+  question.textContent = "What is it the correct term for when mutiple values are stored in a variable?";
+  option1.textContent = "Dataset";
+  option2.textContent = "Array";
+  option3.textContent = "Variable";
+  option4.textContent = "Database";
 
+  //Registers the correct answer depending on what button is clicked.
   option2.addEventListener("click",() => {
-    winner();
-    secondQuestion();
+      correctAnswer();
+      setTimeout(()=>{
+      outcome.textContent = "";
+      secondQuestion();
+    }, 1000)
+    
+    
   });
 
+  //Registers the incorrect answer depending on what button is clicked.
   [option1,option3,option4].forEach(question1false=> {
     question1false.addEventListener("click", () => {
-      loser();
-      secondQuestion();
+      wrongAnswer();
+      setTimeout(()=>{
+        outcome.textContent = "";
+        secondQuestion();
+      }, 1000)
   })})
 }
 
+// Second question
 function secondQuestion() {
-  question.textContent = "2+2 =";
-  option1.textContent = "3";
-  option2.textContent = "5";
-  option3.textContent = "4";
-  option4.textContent = "6";
+
+  //Question and options
+  document.getElementById("group1").style.display= "none";
+  document.getElementById("group2").style.display= "block";
+  question.textContent = "What's the best way to complete a certain action when a button is clicked using Javascript?";
+  option01.textContent = "If statement";
+  option02.textContent = "Functions";
+  option03.textContent = "Event listener";
+  option04.textContent = "All of the above";
   
-  option3.addEventListener("click",() => {
-    winner();
-   thirdQuestion();
+  //Registers the correct answer depending on what button is clicked.
+  option03.addEventListener("click",() => {
+    correctAnswer();
+    setTimeout(()=>{
+      outcome.textContent = "";
+      thirdQuestion();
+    }, 1000);
   });
 
-  [option1,option2,option4].forEach(question2false=> {
-    question2false.addEventListener("click", () => {
-      loser();
-      thirdQuestion();
+  //Registers the incorrect answer depending on what button is clicked.
+  [option01,option02,option04].forEach(question02false=> {
+    question02false.addEventListener("click", () => {
+      wrongAnswer();
+      setTimeout(()=>{
+        outcome.textContent = "";
+        thirdQuestion();
+      }, 1000);
   })})  
 }
 
+//Third question
 function thirdQuestion() {
-  question.textContent = "3+3 =";
-  option1.textContent = "33";
-  option2.textContent = "4";
-  option3.textContent = "5";
-  option4.textContent = "6";
+  document.getElementById("group2").style.display= "none";
+  document.getElementById("group3").style.display= "block";
+  question.textContent = "What does JSON stand for?";
+  option001.textContent = "Jason";
+  option002.textContent = "Javascript's Son";
+  option003.textContent = "JavaScript Object Notifier";
+  option004.textContent = "JavaScript Object Notation";
 
-  option4.addEventListener("click", winner);
-  [option1,option2,option3].forEach(question1false=> {
-    question1false.addEventListener("click", loser);
+  //Registers the correct answer depending on what button is clicked.
+  option004.addEventListener("click",() => {
+    correctAnswer();
+    setTimeout(()=>{
+      outcome.textContent = "";
+      finalScore();
+    }, 1000);
+  });
+
+  //Registers the incorrect answer depending on what button is clicked.
+  [option001,option002,option003].forEach(question001false=> {
+    question001false.addEventListener("click", () => {
+      wrongAnswer();
+      setTimeout(()=>{
+        outcome.textContent = "";
+        finalScore();
+      }, 1000);
   })
+})
 }
-
-
-function winner() {
-  isWin=true;
-}
-
-function loser(){
-  loseGame();
-}
-
-
-// Event listener to start button to call startGame function on click
-start.addEventListener("click", startQuiz);
 
 // Calls init() so that it fires when page opened
 init();
-
-
-
